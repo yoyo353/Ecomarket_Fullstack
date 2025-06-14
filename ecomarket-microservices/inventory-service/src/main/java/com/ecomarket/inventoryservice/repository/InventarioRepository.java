4f57d42f0c74bb91e33158db8bbd7836b42a213a
Benjamin Polanco <115376642+Stellexd@users.noreply.github.com>
2025-06-14 15:39:19 -0400

    codigo completo 14.06

--- ecomarket-microservices/inventory-service/src/main/java/com/ecomarket/inventoryservice/repository/InventarioRepository.java
+++ ecomarket-microservices/inventory-service/src/main/java/com/ecomarket/inventoryservice/repository/InventarioRepository.java
@@ -1,5 +1,13 @@
 package com.ecomarket.inventoryservice.repository;
 
-public class repository {
+import com.ecomarket.inventoryservice.model.Inventario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface InventarioRepository extends JpaRepository<Inventario, Integer> {
+    List<Inventario> findByProductoId(Integer productoId);
+    List<Inventario> findByEstado(String estado);
 }
